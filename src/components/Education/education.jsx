a5898bda49269861b1367d5200ca72b6896f390e
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-05-29 13:29:08 +0600

    edit education page

--- src/components/Education/education.jsx
+++ src/components/Education/education.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 // components
 import SectionTitle from '../Titles/sectionTitle';
-import Subtitle from '../Titles/subtitle';
-import Certificate from './certificate';
+// import Subtitle from '../Titles/subtitle';
+// import Certificate from './certificate';
 // data
-import { certificates } from './myCertificates'
+// import { certificates } from './myCertificates'
 // style
 import './style.css';
 
@@ -19,17 +19,19 @@ const Education = () => {
                 <div className="education-text">
                     <p className="edu-p p1">{t("education.description-a")}</p>
                     <p className="edu-p p2">{t("education.description-b")}</p>
+                    <p className="edu-p p3">{t("education.current")}</p>
                 </div>
                 
-                <div id="subtitle-education">
-                    <Subtitle title={t("education.subtitle")} />
-                </div>
 
-                <div className="certificates-container">
+                {/* <div id="subtitle-education">
+                    <Subtitle title={t("education.subtitle")} />
+                </div> */}
+
+                {/* <div className="certificates-container">
                     {
                         certificates.map( cert => <Certificate key={cert.name} {...cert} /> )
                     }
-                </div>
+                </div> */}
             </div>
         </div>
     )
